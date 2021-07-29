866d9e488427feed99cf3bb700069785ee81a736
Flavio Lucas Fernandes <flaviolukfer@gmail.com>
2021-07-29 14:52:18 -0300

    Initial version of state machine
    I created the state machine. I created two header files to separate the structures and prototypes of functions.

---
+++ structs.h
@@ -0,0 +1,22 @@
+enum class device_state_t
+{
+	stand_by,
+	logging_in,
+	showing_menu,
+	alarming,
+	getting_body_temperature,
+	getting_blood_oxygenation,
+	getting_heart_rate,
+};
+
+struct statefull_port_t
+{
+	const uint8_t port;
+	bool state;
+};
+
+struct sensor_port_t
+{
+	const uint8_t port;
+	float value;
+};
